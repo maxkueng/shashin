fa50bf33458acfce8b8162fb88dcd9c49f4af8c1
Max Kueng <me@maxkueng.com>
2014-09-21 03:01:59 +0200

    validation

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var url = require('url');
 
-var phantomjs = require('phantomjs').path;
 var assign = require('object-assign');
-var base64 = require('base64-stream');
+var Base64Decode = require('base64-stream').Decode;
 var fileUrl = require('file-url');
+var phantomjs = require('phantomjs').path;
 var viewports = require('viewportsizes');
 
 exports = module.exports = rescap;
 
 var defaults = {
-	phantomTimeout: 300000,
+	phantomTimeout: 300,
 	timeout: 30,
 	https: false,
 	delay: 0,
@@ -24,12 +24,13 @@ var defaults = {
 };
 
 function rescap (uri, resolution, options, callback) {
-	var task = assign(defaults, { uri: uri, resolution: resolution }, options);
-	task = prepareTask(task);
+	var task = prepareTask(assign(
+		defaults,
+		{ uri: uri, resolution: resolution },
+		options
+	));
 
-	var stream = takeScreenshot(task);
-
-	return assign(task, { stream: stream });
+	return assign(task, { stream: takeScreenshot(task) });
 }
 
 function prepareTask (task) {
@@ -39,7 +40,7 @@ function prepareTask (task) {
 	if (isFile) {
 		task.uri = fileUrl(task.uri);
 	} else {
-		task.uri = url.parse(task.uri).protocol ? task.uri : protocol + task.uri;
+		task.uri = (!task.uri) ? '' : url.parse(task.uri).protocol ? task.uri : protocol + task.uri;
 	}
 
 	task = assign(task, parseSize(task.resolution));
@@ -63,55 +64,85 @@ function prepareTask (task) {
 			height: newHeight,
 			zoomFactor: newWidth / resolution.width
 		});
-
-		console.log(task);
 	}
 
 	return task;
 }
 
+function validateTask (task) {
+	console.log(task);
+	var uriParts = url.parse(task.uri);
+
+	if (!uriParts.protocol || !uriParts.host) {
+		return new Error('Invalid argument: Missing or invalid URL.');
+	}
+
+	if (!task.width || !task.height) {
+		return new Error('Invalid argument: Missing or invalid resoultion.');
+	}
+
+	if (task.timeout > task.phantomTimeout) {
+		return new Error('Invalid options: phantomTimeout must be greater than timeout.');
+	}
+
+	if (task.delay > task.timeout) {
+		return new Error('Invalid options: timeout must be greater than delay.');
+	}
+}
+
 function takeScreenshot (task) {
-	var proc = spawn(phantomjs, [
-		path.join(__dirname, 'phantom-screenshot.js'),
-		JSON.stringify(task),
-		'--ignore-ssl-errors=true',
-		'--ssl-protocol=any',
-		'--local-to-remote-url-access=true'
-	]);
+	var stream = new Base64Decode();
+	var err = validateTask(task);
 
-	var stream = proc.stdout.pipe(base64.decode());
+	if (err) {
+		process.nextTick(function () {
+			stream.emit('error', err);
+		});
 
-	var killTimer = setTimeout(function () {
-		stream.emit('error', new Error('Process timed out'));
-		proc.kill();
-	}, task.phantomTimeout);
+	} else {
 
-	proc.stderr.on('data', function (data) {
-		stream.emit('error', new Error(data));
-	});
+		var proc = spawn(phantomjs, [
+			path.join(__dirname, 'phantom-screenshot.js'),
+			JSON.stringify(task),
+			'--ignore-ssl-errors=true',
+			'--ssl-protocol=any',
+			'--local-to-remote-url-access=true'
+		]);
 
-	proc.on('exit', function (code) {
-		clearTimeout(killTimer);
-	});
+		proc.stdout.pipe(stream);
+
+		var killTimer = setTimeout(function () {
+			stream.emit('error', new Error('Process timed out'));
+			proc.kill();
+		}, task.phantomTimeout * 1000);
+
+		proc.stderr.on('data', function (data) {
+			stream.emit('error', new Error(data.toString('utf8').trim()));
+		});
+
+		proc.on('exit', function (code) {
+			clearTimeout(killTimer);
+		});
+	}
 
 	return stream;
 }
 
 function parseSize (size) {
-	if (typeof size === 'string') {
+	if (size && typeof size === 'string') {
 		if (/^\d+x\d+$/i.test(size)) {
 			size = size.split(/x/i);
 		} else {
 			var vp = viewports.get(size);
-			size = vp.size || null;
+			size = (vp) ? vp.size || null : null;
 		}
 	}
 
-	if (Array.isArray(size) && size.length === 2) {
+	if (size && Array.isArray(size) && size.length === 2) {
 		return { width: size[0], height: size[1] };
 	}
 
-	if (typeof size === 'object' && size.hasOwnProperty('width') && size.hasOwnProperty('height')) {
+	if (size && typeof size === 'object' && size.hasOwnProperty('width') && size.hasOwnProperty('height')) {
 		return size;
 	}
 
