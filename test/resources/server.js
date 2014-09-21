5797ac6f6f69df0c0a9af4110baf690d3465691d
Max Kueng <me@maxkueng.com>
2014-09-22 00:24:07 +0200

    added tests

---
+++ test/resources/server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var http = require('http');
+
+exports = module.exports = function (port, timeout) {
+	var app = express();
+
+	app.get('/test/normal', function (req, res) {
+		res.type('html');
+		res.send('<!DOCTYPE html>\n<html><body><div id="test" style="width: 400px; height:300px; margin: 100px auto; background-color: #f00; ">HELLO</div></body></html>');
+	});
+
+	app.get('/test/lag', function (req, res) {
+		var lag = parseInt(req.param('lag'));
+		setTimeout(function () {
+			res.send({ lag: 3 });
+		}, lag * 1000);
+	});
+
+	app.get('/test/slow', function (req, res) {
+		var lag = parseInt(req.param('lag'));
+
+		var interval = setInterval(function () {
+			res.write(String(Math.random()));
+		}, 100);
+
+		setTimeout(function () {
+			console.log('END');
+			clearInterval(interval);
+			res.end('\n');
+		}, lag * 1000);
+	});
+
+	app.get('/test/long', function (req, res) {
+		res.type('html');
+
+		res.write('<!DOCTYPE html>\n<html>\n<body>\n');
+		for (i = 0; i < 100; i++) {
+			res.write('<div style="width: 400px; height:300px; margin: 10px auto; background-color: #f00; ">HELLO</div>\n');
+		}
+		res.write('</html>\n</body>');
+
+		res.end('\n');
+	});
+
+	var server = http.createServer(app);
+	server.listen(port);
+
+	if (timeout) {
+		setTimeout(function () {
+			try {
+				server.close();
+			} catch (e) {
+			}
+		}, timeout * 1000);
+	}
+
+	return server;
+};
---
+++ test/shashin.js
@@ -0,0 +1,205 @@
+var test = require('tape');
+var shashin = require('../index');
+var fs = require('fs');
+var path = require('path');
+var imageSize = require('image-size');
+var wrappy = require('wrappy');
+
+var once = wrappy(function (callback) {
+	var called = false
+	return function () {
+		if (called) { return; }
+		called = true
+		return callback.apply(this, arguments);
+	}
+});
+
+var server = require('./resources/server');
+
+test('VALIDATION', function (t) {
+
+	t.test('should emit error when URL is missing', function (tt) {
+		tt.plan(1);
+	
+		var info = shashin(undefined, '1024x768');
+
+		info.stream.on('error', once(function (err) {
+			tt.ok(err instanceof Error, 'emits error');
+		}));
+	});
+
+	t.test('should send error when resolution is invalid', function (tt) {
+		tt.plan(1);
+	
+		var info = shashin('example.com', 'my ass', {});
+
+		info.stream.on('error', once(function (err) {
+			tt.ok(err instanceof Error, 'emits error');
+		}));
+	});
+
+	t.test('should send error when delay is greater than timeout', function (tt) {
+		tt.plan(1);
+	
+		var info = shashin('example.com', '1024x768', { delay: 40, timeout: 20 });
+
+		info.stream.on('error', once(function (err) {
+			tt.ok(err instanceof Error, 'emits error');
+		}));
+	});
+
+	t.test('should send error when phantomTimeout is too low', function (tt) {
+		tt.plan(1);
+	
+		var info = shashin('example.com', '1024x768', { timeout: 20, phantomTimeout: 10 });
+
+		info.stream.on('error', once(function (err) {
+			tt.ok(err instanceof Error, 'emits error');
+		}));
+	});
+
+	t.test('should send error when URL couldn\'t be loaded', function (tt) {
+		tt.plan(1);
+	
+		var info = shashin('exxxxxxxxxxxxxxxxample.com', '1024x768');
+
+		info.stream.on('error', once(function (err) {
+			tt.ok(err instanceof Error, 'emits error');
+		}));
+	});
+
+});
+
+test('OPTIONS', function (t) {
+
+	t.test('timeout: should send error if request timed out', function (tt) {
+		tt.plan(2);
+
+		var startTime = +Date.now();
+		var s = server(13371, 3);
+	
+		var info = shashin('http://localhost:13371/test/lag?lag=3', '1024x768', { timeout: 2 });
+
+		info.stream.on('error', once(function (err) {
+			var timeElapsed = Math.round((+Date.now() - startTime) / 1000);
+
+			s.close();
+
+			tt.ok(err instanceof Error, 'emits error');
+			tt.equal(timeElapsed, 2, 'timeout is correct');
+			tt.end();
+		}));
+
+	});
+
+	t.test('delay: should delay the capture', function (tt) {
+		tt.plan(1);
+
+		var delay = 2;
+		var startTime = +Date.now();
+		var s = server(13371, 4);
+	
+		var info = shashin('http://localhost:13371/test/normal', '1024x768', { delay: delay });
+
+		info.stream.on('finish', function () {
+			var timeElapsed = Math.round((+Date.now() - startTime) / 1000);
+
+			s.close();
+
+			tt.ok(timeElapsed >= delay, 'delay is correct');
+			tt.end();
+		});
+	});
+
+	t.test('crop: should crop to resolution', function (tt) {
+		tt.plan(2);
+
+		var s = server(13371, 2);
+		var filePath = 'test.png';
+	
+		var info = shashin('http://localhost:13371/test/long', '1024x768', { crop: true });
+
+		var file = fs.createWriteStream(filePath);
+
+		file.on('finish', function () {
+			var dims = imageSize(filePath);
+			tt.equal(dims.width, 1024, 'width ok');
+			tt.equal(dims.height, 768, 'height ok');
+
+			fs.unlinkSync(filePath);
+		});
+
+		info.stream.pipe(file);
+
+		info.stream.on('finish', function () {
+			s.close();
+		});
+	});
+
+	t.test('crop: should capture the full page without crop', function (tt) {
+		tt.plan(2);
+
+		var s = server(13371, 4);
+		var filePath = 'test.png';
+	
+		var info = shashin('http://localhost:13371/test/long', '1024x768');
+
+		var file = fs.createWriteStream(filePath);
+
+		file.on('finish', function () {
+			var dims = imageSize(filePath);
+			tt.equal(dims.width, 1024, 'width ok');
+			tt.ok(dims.height > 768, 'height ok');
+
+			fs.unlinkSync(filePath);
+		});
+
+		info.stream.pipe(file);
+
+		info.stream.on('finish', function () {
+			s.close();
+		});
+	});
+
+	t.test('selector: should capture only the selected element', function (tt) {
+		tt.plan(2);
+
+		var s = server(13371, 2);
+		var filePath = 'test.png';
+	
+		var info = shashin('http://localhost:13371/test/normal', '1024x768', { selector: '#test' });
+
+		var file = fs.createWriteStream(filePath);
+
+		file.on('finish', function () {
+			var dims = imageSize(filePath);
+			tt.equal(dims.width, 400, 'width ok');
+			tt.equal(dims.height, 300, 'height ok');
+
+			fs.unlinkSync(filePath);
+		});
+
+		info.stream.pipe(file);
+
+		info.stream.on('finish', function () {
+			s.close();
+		});
+	});
+
+	t.test('selector: should send error if element not found', function (tt) {
+		tt.plan(1);
+
+		var s = server(13371, 3);
+	
+		var info = shashin('http://localhost:13371/test/normal', '1024x768', { selector: '#notexists' });
+
+		info.stream.on('error', once(function (err) {
+			s.close();
+
+			tt.ok(err instanceof Error, 'emits error');
+			tt.end();
+		}));
+
+	});
+
+});
